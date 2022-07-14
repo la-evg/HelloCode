7799406df2b2b6a7771a06a9d3869729604a0f2f
Tema_Larin <artem_larin@yoniti.ru>
2022-07-14 15:58:17 +0300

    add Example010_MethodArray project

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 5, 6, 8, 7, 4, 12, 6, 9, 8 };
+int n = array.Length;
+int find = 8;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
